dd4c58d2aa5f9ee537dadad587634b192ff62358
Nathan Taing <ntaing24@students.polytechnic.org>
2024-01-09 11:59:54 -0800

    added name

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,10 @@
+import java.util.*;
+
 public class HelloWorld{
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("What is your name?: ");
+        String name = input.next();
+        System.out.println("Hello, " + name);
     }
 }
